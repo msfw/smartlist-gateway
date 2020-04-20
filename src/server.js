d6652899af056615ae371018ee404efd95538589
Andrei Franz Salvador <andrei.salvador@lincros.com>
2020-04-20 16:28:21 -0300

    Alteração no gateway

--- src/server.js
+++ src/server.js
@@ -3,22 +3,27 @@ const httpProxy = require('express-http-proxy');
 const cors = require('cors');
 const path = require('path');
 const middleware = require('./middlewares/middleware');
+const helmet = require('helmet')
 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
-const app = express();
+const app = express()
 
-const userServiceProxy = httpProxy(process.env.AUTH_API_URL);
-const listsServiceProxy = httpProxy(process.env.LISTS_API_URL);
-
+app.use(helmet())
 const site = process.env.ALLOW_FROM_URL;
 app.use(cors({
     origin: site,
     optionsSuccessStatus: 200    
 }));
-//app.get('/', (req, res) => res.send('Hello Gateway API'));
+
+const userServiceProxy = httpProxy(process.env.AUTH_API_URL);
+const listsServiceProxy = httpProxy(process.env.LISTS_API_URL);
 
 app.post('/auth/*', (req, res, next) => userServiceProxy(req, res, next));
+app.get('/auth/*', (req, res, next) => userServiceProxy(req, res, next));
+app.put('/auth/*', (req, res, next) => userServiceProxy(req, res, next));
+app.delete('/auth/*', (req, res, next) => userServiceProxy(req, res, next));
+
 
 app.get('/list*', (req, res, next) => middleware(req, res, () => listsServiceProxy(req, res, next)));
 app.put('/list*', (req, res, next) => middleware(req, res, () => listsServiceProxy(req, res, next)));
