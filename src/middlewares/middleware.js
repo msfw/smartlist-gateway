50379f5cb7b1d169fa798ad6f747c44304287fef
Andrei F. Salvador <andreifs95@gmail.com>
2020-04-25 18:18:45 -0300

    Gateway explore

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = function (req, res, next) {
+    console.log(req.headers)
     const authHeader = req.headers.authorization;
     const { noTokenProvided, tokenIncorrectFormat, malFormattedToken, invalidToken } = res.__('tokenErrors');
 
