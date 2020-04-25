d2cf7318e2553d989a55768947a12cb5962143f0
Andrei F. Salvador <andreifs95@gmail.com>
2020-04-25 18:40:22 -0300

    Gateway explore

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -6,25 +6,31 @@ module.exports = function (req, res, next) {
     console.log(`Token: ${authHeader}`)
     console.log(`Token valid: ${(!authHeader)}`)
     const { noTokenProvided, tokenIncorrectFormat, malFormattedToken, invalidToken } = res.__('tokenErrors');
-    console.log("FOI")
+    console.log("Valida existe token")
     if (!authHeader)
         return res.status(noTokenProvided.status).send(noTokenProvided.description)
-
+    console.log("validou")
     const parts = authHeader.split(' ')
 
+    console.log("VAlida parts")
     if (!parts.length === 2)
         return res.status(tokenIncorrectFormat.status).send(tokenIncorrectFormat.description)
-
+    console.log("VAlidou")
     const [schema, token] = parts;
 
+    console.log("Valida bearer")
     if (!/^Bearer$/i.test(schema))
         return res.status(malFormattedToken.status).send(malFormattedToken.description)
+    console.log("Validou")
 
+    console.log("Valida jwt")
     jwt.verify(token, process.env.SHARED_SERVICES_INFO_SECRET, (err, decoded) => {
         if (err) return res.status(invalidToken.status).send(invalidToken.description)
 
         res.userId = decoded.id;
     })
+    console.log("Validou")
 
+    console.log("NEXT")
     next();
 }
